6aef91fac7864b992bd8085fa0cb9c1947605e16
Olarn U <olarn.u@gmail.com>
2020-07-02 09:32:46 +0700

    เพิ่มตัวอน่าง Some keyword

---
+++ RxCode/Playgrounds/Some.playground/Contents.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+enum ShapeType {
+    case circle
+    case square
+}
+
+protocol Shape {
+    associatedtype ShapeColor
+    var color: ShapeColor { get }
+    func describe() -> String
+}
+
+struct Square: Shape {
+    var color: String
+    func describe() -> String {
+        return "I'm Square with \(color) color."
+    }
+}
+
+struct Circle: Shape {
+    var color: Int
+    func describe() -> String {
+        return "I'm Circle with \(color) color code."
+    }
+}
+
+// error Protocol 'Shape' can only be used as a generic constraint because it has Self or associated type requirements
+// if not specific 'some' keyword
+func getSquare() -> some Shape {
+    return Square(color: "red")
+}
+
+func getCircle() -> some Shape {
+    return Circle(color: 127)
+}
+
+let s = getSquare()
+print(s.describe())
+
+let c = getCircle()
+print(c.describe())
